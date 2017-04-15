537e6d936699fcfc738532de4e10d2f3328a8d54
langens-jonathan <flowofcontrol@gmail.com>
2017-04-15 13:41:26 +0200

    Cleaned up the code and added JSON API response

--- web.rb
+++ web.rb
@@ -1,33 +1,66 @@
-post "/upload" do
+post "/files" do
   file_uuid = generate_uuid()
+  file_name = "/files" + file_uuid
+  original_file_name = params['file'][:filename]
+  uploaded_at = Time.now.to_s
+  status = "uploaded"
 
-  file_name = "/data/uploads" + file_uuid
+  copy_file_from_to(params['file'][:tempfile], file_name)
 
-  temp_file = File.open(params['file'][:tempfile], "r") do |f|
-    File.new(file_name, "w")
-    File.open(file_name, "w") do |c|
+  store_file_object_in_triple_store(file_uuid, file_name, original_file_name, uploaded_at, status)
+  
+  generate_JSON_API(file_uuid, file_name, original_file_name, uploaded_at, status)
+end
+
+
+# copies a file object to the destination path
+def copy_file_from_to(original_file, destination)
+  temp_file = File.open(original_file, "r") do |f|
+    File.new(destination, "w")
+    File.open(destination, "w") do |c|
       c.write(f.read())
     end
   end
+end
 
-  file_uuid = generate_uuid()
+# stores the information about the file in the triple store
+def store_file_object_in_triple_store(file_uuid, file_name, original_file_name, uploaded_at, status)
+  update(generate_query(file_uuid, file_name, original_file_name, uploaded_at, status))
+end
 
+# generates a query to persist the file informatino in the triple store
+def generate_query(file_uuid, file_name, original_file_name, uploaded_at, status)
   file_uri = ENV['FILE_SERVICE_BASE_URI'] + "/" + file_uuid
 
-  query = "WITH <" + ENV['MU_APPLICATION_GRAPH'] + "> INSERT DATA {\n"
+  query = "WITH <" + ENV['MU_APPLICATION_GRAPH'] + "> INSERT DATA {"
 
-  query += "<" + file_uri + "> a <" + ENV['FILE_SERVICE_FILE_CLASS'] + ">;\n"
+  query += "<" + file_uri + "> a <" + ENV['FILE_SERVICE_FILE_CLASS'] + ">;"
 
-  query += "<#{MU_CORE}/uuid> \"" + file_uuid + "\";\n"
+  query += "<#{MU_CORE}/uuid> \"" + file_uuid + "\";"
 
-  query += "<" + ENV['FILE_SERVICE_FILE_NAME'] + "> \"" + file_name + "\" ;\n"
+  query += "<" + ENV['FILE_SERVICE_FILE_NAME'] + "> \"" + file_name + "\" ;"
 
-  query += "<" + ENV['FILE_SERVICE_FILE_STATUS'] + "> \"uploaded\" .\n"
+  query += "<" + ENV['FILE_SERVICE_ORIGINAL_FILE_NAME'] + "> \"" + original_file_name + "\" ;"
+
+  query += "<" + ENV['FILE_SERVICE_UPLOADED_AT'] + "> \"" + uploaded_at + "\" ;"
+
+  query += "<" + ENV['FILE_SERVICE_FILE_STATUS'] + "> \"" + status + "\" ."
 
   query += "}"
+end
 
-  update(query)
-
-  "OK"
-  
+# generates a JSON API representation of the created resource
+def generate_JSON_API(file_uuid, file_name, original_file_name, uploaded_at, status)
+  JSON.generate({:data =>
+                 {
+                   :type => "file",
+                   :id => file_uuid,
+                   :attributes =>
+                   {
+                     :original_file_name => original_file_name,
+                     :uploaded_at => uploaded_at,
+                     :status => status,
+                   }
+                 }
+                })
 end
